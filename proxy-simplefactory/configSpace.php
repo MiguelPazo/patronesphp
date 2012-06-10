6fd98795c3bf6ad59c936891922967ca88244e49
Miguel Rodrigo Pazo Sanchez <miguel.ps19@gmail.com>
2012-06-10 17:07:06 -0500

    Barra / borrada

--- proxy-simplefactory/configSpace.php
+++ proxy-simplefactory/configSpace.php
@@ -1,4 +1,4 @@
 <?php
 $_SESSION['HOME']= $_SERVER[DOCUMENT_ROOT];
 $_SESSION['APLICATION']= $_SESSION['HOME']."/patrones.git/proxy-simplefactory/";
-$_SESSION['CLASS']= $_SESSION['APLICATION']."/class/";
+$_SESSION['CLASS']= $_SESSION['APLICATION']."class/";
